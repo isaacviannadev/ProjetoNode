371921473ae055afa3e9f14ebd40e87d869f00f7
Isaac Vianna <isaacsvianna@gmail.com>
2020-09-09 22:10:12 -0300

    Finalizando Modulo 1 node

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
 app.get('/', (request, response) =>
   response.json({ message: 'Hello GoStack' }),
 );
